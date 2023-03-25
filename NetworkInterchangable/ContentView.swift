b61cfb7baf8403a2c88cff618f7721120b5df4b4
mkadr5 <muhammetkadir52@gmail.com>
2023-03-25 13:48:33 +0300

    Initial Commit

---
+++ NetworkInterchangable/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  NetworkInterchangable
+//
+//  Created by Muhammet Kadir on 25.03.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NetworkInterchangable/NetworkInterchangableApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkInterchangableApp.swift
+//  NetworkInterchangable
+//
+//  Created by Muhammet Kadir on 25.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct NetworkInterchangableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
